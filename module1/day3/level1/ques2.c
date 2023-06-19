edb2128c9956ab60c0e5f506bb82578e21144de8
Simranjaiswal09 <92321888+Simranjaiswal09@users.noreply.github.com>
2023-06-19 16:09:27 +0530

    sol2

---
+++ module1/day3/level1/ques2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void printBits(uint32_t num) {
+    for (int i = 31; i >= 0; i--) {
+        uint32_t mask = 1 << i;
+        int bit = (num & mask) ? 1 : 0;
+        printf("%d", bit);
+    }
+    printf("\n");
+}
+
+int main() {
+    uint32_t num;
+
+    printf("Enter a 32-bit integer: ");
+    scanf("%u", &num);
+
+    printf("Binary representation: ");
+    printBits(num);
+
+    return 0;
+}
